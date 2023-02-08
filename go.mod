7556208e9f7fe9297d14dbcaa70b52bcdecaea25
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-02-08 08:31:26 -0800

    update router package to reference the latest version

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/cisagov/con-pca-tasks
 
 go 1.20
 
-require github.com/go-chi/chi v1.5.4
+require github.com/go-chi/chi/v5 v5.0.8
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/cisagov/con-pca-tasks/controllers"
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
