9fd233cb37db70974ca0befe1c0479f6f1050485
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-02-15 12:06:59 -0800

    feature: Add Send Notification Email Endpoint

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,11 +1,39 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/cisagov/con-pca-tasks/aws"
+	"github.com/go-chi/chi/v5"
 )
 
-// HealthCheckHandler indicates that the server is up and running.
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+// healthCheckHandler indicates that the server is up and running.
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Up and running!")
+}
+
+// notificationEmailsHandler manages notificaiton emails.
+func notificationEmailsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+
+	var e aws.SESEmail
+	decoder.Decode(&e)
+
+	email := aws.NewSESEmail()
+	email.BuildMessage(e.To, e.Cc, e.Bcc, e.Subject, e.Body)
+	email.Send()
+
+	fmt.Fprintf(w, "Notification email sent!")
+}
+
+func TasksRouter() http.Handler {
+	r := chi.NewRouter()
+	r.Get("/", healthCheckHandler)
+	r.Post("/notifications/send", notificationEmailsHandler)
+
+	return r
 }
--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 func TestHealthCheckHandler(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req := httptest.NewRequest(http.MethodGet, "/tasks", nil)
 	w := httptest.NewRecorder()
-	HealthCheckHandler(w, req)
+	healthCheckHandler(w, req)
 	res := w.Result()
 	defer res.Body.Close()
 	data, err := io.ReadAll(res.Body)
@@ -19,5 +19,20 @@ func TestHealthCheckHandler(t *testing.T) {
 	}
 	if string(data) != "Up and running!" {
 		t.Errorf("expected 'Up and running!', but got %v", string(data))
+	}
+}
+
+func TestNotificationEmailsHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+	w := httptest.NewRecorder()
+	notificationEmailsHandler(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) != "Notification email sent!" {
+		t.Errorf("expected 'Notification email sent!', but got %v", string(data))
 	}
 }
