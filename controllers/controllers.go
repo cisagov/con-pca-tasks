48e2d4fac4496e0aa1b0d30078758e809400ac83
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-02-27 10:50:08 -0800

    add two more endpoints

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -31,9 +31,31 @@ func notificationEmailsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Notification email sent!")
 }
 
+// emailReportHandler manages emailing notification reports.
+func emailReportHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	cycleId := chi.URLParam(r, "cycle_id")
+	reportType := chi.URLParam(r, "report_type")
+
+	fmt.Fprintf(w, "%s report email sent! Cycle id: %s", reportType, cycleId)
+}
+
+// pdfReportHandler manages PDF reports.
+func pdfReportHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	cycleId := chi.URLParam(r, "cycle_id")
+	reportType := chi.URLParam(r, "report_type")
+
+	fmt.Fprintf(w, "%s report pdf complete! Cycle id: %s", reportType, cycleId)
+}
+
 func TasksRouter() http.Handler {
 	r := chi.NewRouter()
 	r.Get("/", healthCheckHandler)
+	r.Get("/{cycle_id}/reports/{report_type}/email", emailReportHandler)
+	r.Get("/{cycle_id}/reports/{report_type}/pdf", pdfReportHandler)
 	r.Post("/notifications/send", notificationEmailsHandler)
 
 	return r
