9d620c54dca9e36ca4cd592d6b2ccabef9b219ec
itsmostafa <mostafaxcodes@gmail.com>
2023-03-07 22:48:15 -0800

    feature: add notification emails manager

--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -22,17 +22,22 @@ func TestHealthCheckHandler(t *testing.T) {
 	}
 }
 
-func TestNotificationEmailsHandler(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/tasks", nil)
-	w := httptest.NewRecorder()
-	notificationEmailsHandler(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
-	}
-	if string(data) != "Notification email sent!" {
-		t.Errorf("expected 'Notification email sent!', but got %v", string(data))
-	}
-}
+// func TestEmailReportHandler(t *testing.T) {
+// 	req := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+// 	w := httptest.NewRecorder()
+
+// 	rctx := chi.NewRouteContext()
+// 	rctx.URLParams.Add("cycle_id", "test-cycle-id")
+// 	rctx.URLParams.Add("report_type", "cycle")
+
+// 	emailReportHandler(w, req)
+// 	res := w.Result()
+// 	defer res.Body.Close()
+// 	data, err := io.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Errorf("expected error to be nil got %v", err)
+// 	}
+// 	if string(data) != "cycle report email sent! Cycle id: test-cycle-id" {
+// 		t.Errorf("expected 'cycle report email sent! Cycle id: test-cycle-id', but got %v", string(data))
+// 	}
+// }
